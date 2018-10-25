91eaef5a6c9e9d9d58f17a53280119f447593851
willyycn <willyy.cn@gmail.com>
2018-10-25 14:50:46 +0800

    change demo port

--- Android/app/src/main/java/com/rsun/andemo/Operation/NetOpHelper.java
+++ Android/app/src/main/java/com/rsun/andemo/Operation/NetOpHelper.java
@@ -21,7 +21,7 @@ import retrofit2.Retrofit;
 
 public class NetOpHelper {
     private static final String TAG = "NetOpHelper";
-    public static final String ServerUrl = "http://10.10.252.13:3000";
+    public static final String ServerUrl = "http://10.10.252.13:8000";
     public static final String ServerParam = "fca9952d08b178ffcf8ee03750e425f63a68e32d24f38e26e65f59dc8a056460545ed89a8c175dda576d62dc1d6ba3e092b5a34495525de1c71961ec11b7dbea";
     public static final String ServerToken = "{\"neo_server\":\"willyy@R-Sun-X\"}";
     public static final String ServerChkSum = "a2c51e7056bc315df8ce67d8330564803598ac10909c53f10f33fd3c7aba46603add8883c4ea88b5bb6c778f00e47836470918b720e92a068db0005a6c0df643";
